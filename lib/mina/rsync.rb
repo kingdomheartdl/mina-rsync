e0359e843ea3948168466ca687ee42e451dccfab
HienNguyenVan <vanhienk208tt@gmail.com>
2018-08-16 02:47:23 +0700

    Update rsync.rb
    Fix error

--- lib/mina/rsync.rb
+++ lib/mina/rsync.rb
@@ -72,8 +72,8 @@ namespace :rsync do
   end
 
   task :build do
-    run %(echo "-> Copying from cache directory to build path")
-    run %(#{settings.rsync_copy} "#{rsync_cache.call}/" ".")
+    comment 'echo "-> Copying from cache directory to build path"'
+    run "#{settings.rsync_copy} \"#{rsync_cache.call}/\" \".\""
   end
 
   desc "Stage, rsync and copy to the build path."
